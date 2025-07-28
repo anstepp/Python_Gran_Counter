c6523dcd712461729d3577a1a2d32fadd5891326
Aaron Stepp <stepp.aaron@gmail.com>
2025-07-28 14:40:51 -0500

    float test yes

--- test_gran_counter.py
+++ test_gran_counter.py
@@ -5,7 +5,13 @@ from gran_counter import CircularBuffer
 test_buffer = [0.0, 0.1, 0.2, 0.3]
 
 def test_gran_init():
+    """
+        Test init function properly takes in the buffer and
+        float-ifys it.
+    """
     TestBuffer = CircularBuffer(test_buffer)
+    for supposed_to_be_float in TestBuffer.samples:
+        assert type(supposed_to_be_float) == float
 
 def test_increment_input():
     pass
