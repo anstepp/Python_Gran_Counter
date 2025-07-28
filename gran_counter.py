6efb0bb09d52c50c55e192711a5c957466b719a2
Aaron Stepp <stepp.aaron@gmail.com>
2025-07-27 22:02:52 -0500

    initial gran counter

---
+++ gran_counter.py
@@ -0,0 +1,19 @@
+#set up a circular buffer object
+
+class CircularBuffer:
+    
+    def __init__(self):
+        pass
+
+
+    def _advance_input_counter(self):
+        pass
+
+    def _advance_output_counter(self):
+        pass
+
+    def _read_output(self):
+        pass
+
+    def _read_input(self):
+        pass
