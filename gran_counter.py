f7b5f4c24a2d551f4922f1a43ef48bcd26a9c484
Aaron Stepp <stepp.aaron@gmail.com>
2025-07-27 22:37:55 -0500

    finished rough draft

--- gran_counter.py
+++ gran_counter.py
@@ -3,17 +3,27 @@
 class CircularBuffer:
     
     def __init__(self):
-        pass
+        self.samples = []
+        self.input_counter = 0
+        self.output_counter = 0
 
 
     def _advance_input_counter(self):
-        pass
+        if input_counter < len(samples):
+            input_counter += 1
+        else:
+            input_counter = 0
 
     def _advance_output_counter(self):
-        pass
+        if output_counter < len(samples):
+            output_counter += 1
+        else:
+            output_counter = 0
 
-    def _read_output(self):
-        pass
+    def read_output(self):
+        self._advance_output_counter()
+        return samples[output_counter]
 
-    def _read_input(self):
-        pass
+    def _read_input(self, read_sample):
+        samples[input_counter] = read_sample
+        self._advance_input_counter()
