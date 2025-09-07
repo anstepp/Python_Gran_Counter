b98e421533eaa1dd264bdb266978c13b8d4cdcc2
Aaron Stepp <stepp.aaron@gmail.com>
2025-09-07 11:30:46 -0500

    with formulas for windows

--- granulator.py
+++ granulator.py
@@ -1,26 +1,43 @@
+from math import pi, cos, sin, degrees, radians
 
+from circular_buffer import CircularBuffer
+
+class WindowType(Enum):
+    HANN = 1
+    HAMM = 2
+    RECTANGULAR = 3
+    TRAPEZOID = 4
 
 class Window:
 
     def __init__(self, window_type: str, buffer: list[float]) -> None:
+
         self.window_type = window_type
         self.window_index = 0
+        self.window_buffer = CircularBuffer(buffer)
 
-    def _advance_window_index(self):
-        pass
+    def _calculate_window(self, window_type: WindowType):
+        match window_type:
+            case 1:
+                return 0.5 * (1 - cos((2 * pi * n)/N))
+            case 2:
+                return 0.54 * 0.46 * cos((2 * pi * n)/(N-1))
+            case 3:
+                return x
+            case 4:
+                pass
 
 
 class Granulator:
 
-    def __init__(self, window: Window):
-        self.buffer = buffer
+    def __init__(self, window: Window, window_size: int, window_type="hann": WindowType):
+        self.buffer = CircularBuffer(buffer)
         self.buffer_index = 0
-        self.window = window
+        self.window = window("hann")
 
     def return_sample(self):
-        sample = self.buffer[self.buffer_index] * self.window.samples[self.window.window_index]
+        
+        sample = self.buffer.read_index() * self.window.window_buffer.read_index()
         
         return (sample)
 
-    def _advance_buffer_index(self):
-        pass
--- test_granulator.py
+++ test_granulator.py
@@ -1,6 +1,28 @@
 import pytest
 
-from granulator import Granulator
+from granulator import Granulator, Window
 
-def test_gran_init():
+@pytest.fixture
+def sample_window():
+    sample_window = [0, 1, 2]
+    yield sample_window
+
+@pytest.fixture
+def sample_gran():
+    sample_gran = [0, 1, 2]
+    yield sample_gran
+
+@pytest.fixture
+def sample_objects(sample_window, sample_gran):
+    window_object = Window(sample_window)
+    granulator_object = Granulator(sample_gran)
+    yield window_object, granulator_object
+
+def test_gran_init(sample_window, sample_gran):
+    pass
+
+def test_window_init(sample_window, sample_gran):
+    pass
+
+def test_return_sample(sample_window, sample_gran):
     pass
